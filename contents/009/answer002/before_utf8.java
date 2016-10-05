767eeaba7dd9b644b35df7881ef25bbb2416372e
Fukuda Taiki <taiki.fukuda@justsystems.com>
2016-10-05 11:01:30 +0900

    Q9の修正を行いました。 修正内容：利用するプロパティファイルにコメント文を記載し、プロパティファイルの内容を正しく修正。

--- contents/009/answer002/before_utf8.java
+++ contents/009/answer002/before_utf8.java
@@ -1,5 +1,14 @@
+/**
+ * 009で使用するファイルです。
+ *
+ * @author jsfkdt
+ */
 class before_utf8{
+    /**
+     * 009で使用するファイルです。
+     * @param arguments 使用しません。
+     */
   public static void main(String args[]){
-    System.out.println("あいうえお");
+    System.out.println("山田タロウ");
   }
 }
