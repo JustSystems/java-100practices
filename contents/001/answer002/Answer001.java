ba53fff394c8e27cd79abcd4131524080f9af023
Fukuda Taiki <taiki.fukuda@justsystems.com>
2016-10-05 10:28:35 +0900

    Q1の修正を行いました。 修正内容：コメント文の記載

--- contents/001/answer002/Answer001.java
+++ contents/001/answer002/Answer001.java
@@ -1,4 +1,13 @@
+/**
+ * 001の解答です。
+ *
+ * @author jsfkdt
+ */
 public class Answer001 {
+    /**
+     * 001の解答です。
+     * @param arguments 使用しません。
+     */
     public static void main(String[] arguments) {
         System.out.println("Hello, World!");
     }
