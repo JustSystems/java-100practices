93d7c292947da17f72ca40d0f75b16128eb42669
Fukuda Taiki <taiki.fukuda@justsystems.com>
2016-10-07 11:33:32 +0900

    Q17の解答を修正しました。 修正内容：参照値を評価用変数に代入し、スレッドAにて値を正しく評価可能な形式に修正

--- contents/017/answer002/src/Answer017.java
+++ contents/017/answer002/src/Answer017.java
@@ -1,6 +1,7 @@
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * 017の解答です。
@@ -15,22 +16,32 @@ public class Answer017 {
     /** ある long 変数. */
     private static volatile long variable = -1L;
 
+    /** スレッドAにて評価用の変数 **/
+    private static long variable2;
+    
     /**
      * 017の解答です.
      * @param arguments 使用しません.
      */
     public static void main(final String[] args) throws InterruptedException {
         // 複数スレッドの終了を同期する仕組み
-        final CountDownLatch latch = new CountDownLatch(2); //スレッドが2つとも終了するまで待つためのクラス.
-        final ExecutorService es = Executors.newFixedThreadPool(2); //2つのスレッドを並列処理させるためのクラス.
+        final CountDownLatch latch = new CountDownLatch(2); // スレッドが2つとも終了するまで待つためのクラス.
+        final ExecutorService es = Executors.newFixedThreadPool(2); // 2つのスレッドを並列処理させるためのクラス.
+        final AtomicLong atom = new AtomicLong(); // long型変数をアトミックな値に設定するためのクラス.
 
         // スレッドA.
         es.submit(() -> {
             for (int i = 0; i < COUNT; i++) {
                 
+                // 参照値をアトミックな値に設定.
+                atom.set(variable);
+                
+                // 参照値を評価用変数に代入.
+                variable2 = atom.get();
+                
                 // 変数が0でも-1でもない場合にその内容を出力.
-                if (variable != 0 && variable != -1) {
-                    System.out.printf("variable (%d) is not 0 or -1.\n", variable);
+                if (variable2 != 0 && variable2 != -1) {
+                    System.out.printf("variable (%d) is not 0 or -1.\n", variable2);
                 }
             }
             
@@ -41,7 +52,6 @@ public class Answer017 {
         // スレッドB.
         es.submit(() -> {
             for (int i = 0; i < COUNT; i++) {
-                
                 // 変数が0の場合-1Lを代入し、それ以外の場合は0Lを代入する.
                 if (variable == 0) {
                     variable = -1L;
