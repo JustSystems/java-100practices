89eb6a06f020b7eab06d2ec634294c47fc0e7c88
Fukuda Taiki <taiki.fukuda@justsystems.com>
2016-10-10 15:18:44 +0900

    Q21の解答を修正しました。 修正内容：JavaDocコメント文の記載

--- contents/021/answer/src/RandomRmi.java
+++ contents/021/answer/src/RandomRmi.java
@@ -2,10 +2,15 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 /**
- * 021の解答です. 0～7の任意の整数を返すインターフェイス.
+ * 021の解答です.
  *
  * @author jsfkdt
  */
 public interface RandomRmi extends Remote {
+    
+    /**
+     * 021の解答です.0～7の任意の整数を返します.
+     * @return 0～7の任意の整数（ランダム）
+     */
     int returnRandom() throws RemoteException;
 }
