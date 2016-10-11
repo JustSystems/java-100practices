d2bec76ca0f276676c3e727dc58465852af19b04
Fukuda Taiki <taiki.fukuda@justsystems.com>
2016-10-11 10:46:47 +0900

    Q29を修正しました。 修正内容：tryブロックを1つにし、NullPointerExceptionをcatchするよう修正

--- contents/029/answer/src/Answer029.java
+++ contents/029/answer/src/Answer029.java
@@ -15,28 +15,20 @@ public class Answer029 {
      * @param arguments 使用しません.
      */
     public static void main(final String[] args) {
-        /* ジェネリクスを用いて、呼び出すクラスを初期化. */
-        Class<?> stringBufferClass = null;
-        
-        /* 呼び出すメソッドの初期化. */
-        Method appendMethod = null;
         
         try {
+            /* ジェネリクスを用いて、呼び出すクラスを初期化. */
+            Class<?> stringBufferClass = null;
+            
+            /* 呼び出すメソッドの初期化. */
+            Method appendMethod = null;
+            
             // 使用するクラスを明示し、呼び出す.
             stringBufferClass = Class.forName("java.lang.StringBuffer");
             
             // appendメソッドを使用する.引数はString型であることを明示する.
             appendMethod = stringBufferClass.getMethod("append", String.class);
             
-        } catch (ClassNotFoundException e) {
-            // クラスが存在しない.
-            e.printStackTrace ();
-        } catch (NoSuchMethodException e) {
-            // メソッドが存在しない.
-            e.printStackTrace();
-        }
-        
-        try {
             // StringBufferクラスのインスタンスを生成.
             StringBuffer sb = (StringBuffer) stringBufferClass.newInstance();
             
@@ -51,7 +43,15 @@ public class Answer029 {
             
             // 連結された値の標準出力を行う.
             System.out.println(sb.toString());
-            
+        } catch (NullPointerException e) {
+            // nullエラー.
+            e.printStackTrace ();
+        } catch (ClassNotFoundException e) {
+            // クラスが存在しない.
+            e.printStackTrace ();
+        } catch (NoSuchMethodException e) {
+            // メソッドが存在しない.
+            e.printStackTrace();
         } catch (InstantiationException e) {
             // インスタンス作成不可.
             e.printStackTrace();
