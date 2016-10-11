2250247b2f5366c8efec0e62df775f7322e1b60f
Fukuda Taiki <taiki.fukuda@justsystems.com>
2016-10-11 21:47:16 +0900

    Q32～34を解答しました。

---
+++ contents/032/answer/src/Validator.java
@@ -0,0 +1,14 @@
+/**
+ * 032‚Å—p‚¢‚é3‚Ì”{””»’è‘•’u.
+ *
+ * @author jsfkdt
+ */
+interface Validator {
+    /**
+     * 3‚Ì”{”‚©‚Ç‚¤‚©‚Ì”»’èŒ‹‰Ê‚ğ•Ô‹p.
+     *
+     * @param number w’è‚Ìˆø”
+     * @return booleanŒ^
+     */
+    public boolean isValid(int a);
+}
