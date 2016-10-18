b09d3bb177a3adaca8aaba4b3a4324ffda1280b4
Fukuda Taiki <taiki.fukuda@justsystems.com>
2016-10-18 12:49:26 +0900

    Q49の解答を修正しました。 修正内容：nextLong()を使用するよう修正/メインクラスの不要箇所を除去

--- contents/049/answer/src/Answer049.java
+++ contents/049/answer/src/Answer049.java
@@ -6,25 +6,12 @@ import java.util.Date;
  * @author jsfkdt
  */
 public class Answer049 {
-    /* Date型変数. */
-    private Date day;
-    
-    /**
-     * Date型変数に格納する.
-     *
-     * @param day Date型変数
-     */
-    Answer049 (Date day) {
-        this.day = day;
-    }
     
     /**
      * 049の解答です.
      * @param arguments 使用しません.
      */
     public static void main(final String[] args) {
-        // 現在の日付を格納し、Answer049オブジェクトを生成.
-        final Answer049 answer049 = new Answer049(new Date());
         
         // 現在日付から±1日のランダムな日時を取得.
         final Date randomDate = new DateUtil().randomDayAndTime();
--- contents/049/answer/src/DateUtil.java
+++ contents/049/answer/src/DateUtil.java
@@ -1,6 +1,6 @@
 import java.util.Date;
-import java.util.Random;
 import java.util.Calendar;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * 049で使用するユーティリティクラス.
@@ -19,24 +19,31 @@ public final class DateUtil {
         Date randomDate = new Date();
         
         // 一日後の日時.
-        Calendar oneAfterDay = Calendar.getInstance();
+        final Calendar oneAfterDay = Calendar.getInstance();
         oneAfterDay.add(Calendar.DATE, 1);
         
         // 一日前の日時.
-        Calendar oneBeforeDay = Calendar.getInstance();
+        final Calendar oneBeforeDay = Calendar.getInstance();
         oneBeforeDay.add(Calendar.DATE, -1);
+        
+        // ランダムな日時
+        final Calendar randomDay = Calendar.getInstance();
         
         // ミリ秒の差を取得.
         final long timeDiff = oneAfterDay.getTimeInMillis() - oneBeforeDay.getTimeInMillis();
         
         // ミリ差の値をランダムで指定.
-        final int random = new Random().nextInt((int)timeDiff + 1);
+        // ThreadLocalRandomクラスを用いて、指定範囲内のLong型乱数を生成する.
+        final long randomDiffTime = ThreadLocalRandom.current().nextLong(timeDiff + 1L);
         
-        // ランダムな年月日の生成.
-        oneAfterDay.add(oneAfterDay.MILLISECOND, -random);
+        // 1日後のミリ秒からランダム生成したミリ秒を減算.
+        final long randomTime = oneAfterDay.getTimeInMillis() - randomDiffTime;
+        
+        // 取得したミリ秒をカレンダーに設定.
+        randomDay.setTimeInMillis(randomTime);
         
         // Calendar型をDate型へ変換.
-        randomDate = oneAfterDay.getTime();
+        randomDate = randomDay.getTime();
         
         return randomDate;
     }
