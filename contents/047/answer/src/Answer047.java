5367393396507765de6666cb9039886efb314dfa
Fukuda Taiki <taiki.fukuda@justsystems.com>
2016-10-18 09:52:13 +0900

    Q47の解答を修正しました。 修正内容：不要な処理（コンストラクタやインスタンス変数の定義）を削除

--- contents/047/answer/src/Answer047.java
+++ contents/047/answer/src/Answer047.java
@@ -6,28 +6,15 @@ import java.util.Date;
  * @author jsfkdt
  */
 public class Answer047 {
-    /* Date型変数. */
-    private Date day;
-    
-    /**
-     * Date型変数に格納する.
-     *
-     * @param day Date型変数
-     */
-    Answer047 (Date day) {
-        this.day = day;
-    }
     
     /**
      * 047の解答です.
      * @param arguments 使用しません.
      */
     public static void main(final String[] args) {
-        // 現在の日付を格納し、Answer047オブジェクトを生成.
-        final Answer047 answer047 = new Answer047(new Date());
         
         // 定められたフォーマットを返却するユーティリティを使用.
-        final String dayAndTime = new FormatUtil().dayAndTime(answer047.day);
+        final String dayAndTime = new FormatUtil().dayAndTime(new Date());
         
         // 日時を標準出力.
         System.out.println(dayAndTime);
