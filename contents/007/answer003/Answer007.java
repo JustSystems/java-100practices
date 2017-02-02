dcb369f36da4d8f0ae988765eb459a7fc1e98b07
TakuyaMaeda-js <takuya.maeda@justsystems.com>
2017-02-02 17:38:06 +0900

    001から009の初回回答

---
+++ contents/007/answer003/Answer007.java
@@ -0,0 +1,11 @@
+/**
+ * 20170202 takuya.m
+ */
+
+import java.util.Date;
+
+public class Answer007 {
+    public static void main(String arguments[]){
+      System.out.println("Date: " + new Date());
+    }
+}
