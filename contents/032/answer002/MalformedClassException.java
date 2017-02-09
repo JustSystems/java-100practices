c97d5aae1714014ca702e536b783e06bcc04c85a
grindcoffee <takuya.maeda@justsystems.com>
2017-02-09 12:46:38 +0900

    設問032　解答を修正

--- contents/032/answer002/MalformedClassException.java
+++ contents/032/answer002/MalformedClassException.java
@@ -1,8 +1,8 @@
 /**
  * 不正な形式のClassであることを示す例外
  */
-class MalformedClassException extends Exception{
-	public MalformedClassException(String str){
-		super();
-	}
+class MalformedClassException extends Exception {
+    public MalformedClassException(String str) {
+        super(str);
+    }
 }
